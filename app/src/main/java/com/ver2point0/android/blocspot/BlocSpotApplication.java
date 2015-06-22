b962efa21ea3e975798e0e873bf81519d71f51ff
John McIntosh II <jmci10@hotmail.com>
2015-06-22 15:29:28 -0400

    initial commit

---
+++ app/src/main/java/com/ver2point0/android/blocspot/BlocSpotApplication.java
@@ -0,0 +1,12 @@
+package com.ver2point0.android.blocspot;
+
+import android.app.Application;
+
+
+public class BlocSpotApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/ver2point0/android/blocspot/ui/activity/BlocSpotActivity.java
@@ -0,0 +1,15 @@
+package com.ver2point0.android.blocspot.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ver2point0.android.blocspot.R;
+
+
+public class BlocSpotActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blocspot);
+    }
+}
