940baf9c973be277bc14a0b29bb78ef2d3970f95
John McIntosh II <jmci10@hotmail.com>
2015-07-06 18:43:45 -0400

    save points of interest progress

--- app/src/main/java/com/ver2point0/android/blocspot/ui/activity/BlocSpotActivity.java
+++ app/src/main/java/com/ver2point0/android/blocspot/ui/activity/BlocSpotActivity.java
@@ -4,16 +4,22 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
@@ -21,6 +27,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.ver2point0.android.blocspot.R;
 import com.ver2point0.android.blocspot.places.Place;
 import com.ver2point0.android.blocspot.places.PlacesService;
+import com.ver2point0.android.blocspot.util.Constants;
 
 import java.util.ArrayList;
 
@@ -32,11 +39,26 @@ public class BlocSpotActivity extends Activity {
     private String[] mPlaces;
     private LocationManager mLocationManager;
     private Location mLocation;
+    private boolean mListState = true;
+    private MapFragment mMapFragment;
+    private ListView mPoiList;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(Constants.LIST_STATE, mListState);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocspot);
+        if (savedInstanceState != null) {
+            mListState = savedInstanceState.getBoolean(Constants.LIST_STATE);
+        }
+
+        mMapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.f_map);
+        mPoiList = (ListView) findViewById(R.id.lv_poiList);
 
         initCompo();
         mPlaces = getResources().getStringArray(R.array.places);
@@ -58,6 +80,16 @@ public class BlocSpotActivity extends Activity {
                         return true;
                     }
                 });
+
+        if (mListState == true) {
+            getFragmentManager().beginTransaction().hide(getFragmentManager()
+                .findFragmentById(R.id.f_map)).commit();
+        } else if (mListState == false) {
+            mPoiList.setVisibility(View.INVISIBLE);
+        }
+
+
+
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_blocspot);
 //        setSupportActionBar(toolbar);
     } // end method onCreate()
@@ -74,31 +106,6 @@ public class BlocSpotActivity extends Activity {
         }
 
         @Override
-        protected void onPostExecute(ArrayList<Place> result) {
-            super.onPostExecute(result);
-            if (dialog.isShowing()) {
-                dialog.dismiss();
-            }
-            for (int i = 0; i < result.size(); i++) {
-                mGoogleMap.addMarker(new MarkerOptions()
-                .title(result.get(i).getName()
-                .position(new LatLng(result.get(i).getLatitude(),
-                        result.get(i).getLongitude()))
-                .icon(BitmapDescriptorFactory
-                        .fromResource(R.drawable.pin))
-                .snippet(result.get(i).getVicinity())));
-            }
-            CameraPosition cameraPosition = new CameraPosition.Builder()
-                    .target(new LatLng(result.get(0).getLatitude(), result
-                    .get(0).getLongitude()))
-                    .zoom(14)
-                    .tilt(30)
-                    .build();
-            mGoogleMap.animateCamera(CameraUpdateFactory
-                .newCameraPosition(cameraPosition));
-        } // end method onPostExecute()
-
-        @Override
         protected void onPreExecute() {
             super.onPreExecute();
             dialog = new ProgressDialog(context);
@@ -111,7 +118,7 @@ public class BlocSpotActivity extends Activity {
         @Override
         protected ArrayList<Place> doInBackground(Void... arg0) {
             PlacesService service = new PlacesService(
-                getString(R.string.maps_api_key));
+                    getString(R.string.maps_api_key));
             ArrayList<Place> findPlaces = service.findPlaces(mLocation.getLatitude(),
                     mLocation.getLongitude(), places);
 
@@ -122,8 +129,37 @@ public class BlocSpotActivity extends Activity {
             return findPlaces;
         } // end method doInBackground()
 
+        @Override
+        protected void onPostExecute(ArrayList<Place> result) {
+            super.onPostExecute(result);
+            if (dialog.isShowing()) {
+                dialog.dismiss();
+            }
+            for (int i = 0; i < result.size(); i++) {
+                mGoogleMap.addMarker(new MarkerOptions()
+                        .title(result.get(i).getName()
+                                .position(new LatLng(result.get(i).getLatitude(),
+                                        result.get(i).getLongitude()))
+                                .icon(BitmapDescriptorFactory
+                                        .fromResource(R.drawable.pin))
+                                .snippet(result.get(i).getVicinity())));
+            }
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(result.get(0).getLatitude(), result
+                    .get(0).getLongitude()))
+                    .zoom(14)
+                    .tilt(30)
+                    .build();
+            mGoogleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+        } // end method onPostExecute()
+
+
     } // end private class GetPlaces
 
+    private void initCompo() {
+        mGoogleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.f_map)).getMap();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -132,24 +168,64 @@ public class BlocSpotActivity extends Activity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            if (mListState == true) {
+                getFragmentManager().beginTransaction().show(mMapFragment).commit();
+                mPoiList.setVisibility(View.INVISIBLE);
+                mListState = false;
+            } else {
+                getFragmentManager().beginTransaction().hide(mMapFragment).commit();
+                mPoiList.setVisibility(View.VISIBLE);
+                mListState = true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
+    private void currentLocation() {
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        switch (item.getItemId()) {
-//            case R.id.action_one:
-//                break;
-//            case R.id.action_two:
-//                break;
-//            default:
-//                return super.onOptionsItemSelected(item);
-//        }
-//        return false;
-//    }
+        String provider = mLocationManager.getBestProvider(new Criteria(), true);
+
+        Location location = mLocationManager.getLastKnownLocation(provider);
+
+        if (location == null) {
+            mLocationManager.requestLocationUpdates(provider, 0, 0, listener);
+        } else {
+            mLocation = location;
+            new GetPlaces(BlocSpotActivity.this, mPlaces[0].toLowerCase().replace(
+                    "-", "_")).execute();
+            Log.e(TAG, "location : " + location);
+        }
+    }
+
+    private LocationListener listener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.e(TAG, "location update : " + location);
+            mLocation = location;
+            mLocationManager.removeUpdates(listener);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
 
 
 
---
+++ app/src/main/java/com/ver2point0/android/blocspot/util/Constants.java
@@ -0,0 +1,7 @@
+package com.ver2point0.android.blocspot.util;
+
+
+public class Constants {
+
+    public static final String LIST_STATE = "listState";
+}
