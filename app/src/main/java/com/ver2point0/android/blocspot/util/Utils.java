9d202fda9dbe98f7dc3c9ce12890789dd4794a05
John McIntosh II <jmci10@hotmail.com>
2015-07-18 22:54:55 -0400

    mark poi as visited progress

--- app/src/main/java/com/ver2point0/android/blocspot/util/Utils.java
+++ app/src/main/java/com/ver2point0/android/blocspot/util/Utils.java
@@ -14,32 +14,26 @@ public class Utils {
     }
 
     public static void setColorString(String color, TextView colorLabel) {
-        if(color.equals(Constants.CYAN)) {
-            colorLabel.setBackgroundColor(context.getResources().getColor(R.color.cyan));
-        }
-        else if(color.equals(Constants.BLUE)) {
-            colorLabel.setBackgroundColor(context.getResources().getColor(R.color.blue));
-        }
-        else if(color.equals(Constants.GREEN)) {
-            colorLabel.setBackgroundColor(context.getResources().getColor(R.color.green));
-        }
-        else if(color.equals(Constants.MAGENTA)) {
-            colorLabel.setBackgroundColor(context.getResources().getColor(R.color.magenta));
-        }
-        else if(color.equals(Constants.ORANGE)) {
-            colorLabel.setBackgroundColor(context.getResources().getColor(R.color.orange));
-        }
-        else if(color.equals(Constants.RED)) {
-            colorLabel.setBackgroundColor(context.getResources().getColor(R.color.red));
-        }
-        else if(color.equals(Constants.ROSE)) {
-            colorLabel.setBackgroundColor(context.getResources().getColor(R.color.rose));
-        }
-        else if(color.equals(Constants.VIOLET)) {
-            colorLabel.setBackgroundColor(context.getResources().getColor(R.color.violet));
-        }
-        else if(color.equals(Constants.YELLOW)) {
-            colorLabel.setBackgroundColor(context.getResources().getColor(R.color.yellow));
+        if (context != null) {
+            if(color.equals(Constants.CYAN)) {
+                colorLabel.setBackgroundColor(context.getResources().getColor(R.color.cyan));
+            } else if(color.equals(Constants.BLUE)) {
+                colorLabel.setBackgroundColor(context.getResources().getColor(R.color.blue));
+            } else if(color.equals(Constants.GREEN)) {
+                colorLabel.setBackgroundColor(context.getResources().getColor(R.color.green));
+            } else if(color.equals(Constants.MAGENTA)) {
+                colorLabel.setBackgroundColor(context.getResources().getColor(R.color.magenta));
+            } else if(color.equals(Constants.ORANGE)) {
+                colorLabel.setBackgroundColor(context.getResources().getColor(R.color.orange));
+            } else if(color.equals(Constants.RED)) {
+                colorLabel.setBackgroundColor(context.getResources().getColor(R.color.red));
+            } else if(color.equals(Constants.ROSE)) {
+                colorLabel.setBackgroundColor(context.getResources().getColor(R.color.rose));
+            } else if(color.equals(Constants.VIOLET)) {
+                colorLabel.setBackgroundColor(context.getResources().getColor(R.color.violet));
+            } else if(color.equals(Constants.YELLOW)) {
+                colorLabel.setBackgroundColor(context.getResources().getColor(R.color.yellow));
+            }
         }
     }
 }
